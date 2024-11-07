a41c546738f4ffc21db9e5e297d7a54d6d75bbb0
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-07 14:39:41 +0100

    add 500 error middleware

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const notFoundMiddleware = require("./middlewares/notFound.js");
 // json middleware
 app.use(express.json());
 
+// middlware to trigger a 500 error
+app.use("/posts", (req, res, next) => {
+    throw new Error("You broke everythink dude!");
+});
+
 // router middleware
 app.use("/posts", PostsRouter);
 
@@ -27,3 +32,13 @@ app.listen(3001, () => {
 
 // use error middleware
 app.use(notFoundMiddleware);
+
+// 500 error middleware
+app.use((err, req, res, next) => {
+    console.log("Error:", err.message);
+    console.error(err.stack);
+    res.status(500).send({
+        message: "Somethink went wrong",
+        error: err.message
+    });  
+});
